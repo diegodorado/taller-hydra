68a802ec92d72b490c421953b81cc8b655e3a674
Diego Dorado <diegodorado@gmail.com>
2019-11-25 20:51:51 -0300

    edits ejemplos

--- ejemplos.js
+++ ejemplos.js
@@ -19,5 +19,11 @@ voronoi(10,1).colorama(10).hue(0.5).saturate([0,1,4,11]).scale(() => 0.5+0.2*a.f
 
 voronoi(1,10).mult(osc(30,0.51,1.82)).colorama(0.1).rotate(()=>time*0.5).kaleid(8).scale(() => 0.5+0.2*a.fft[0]).out()
 
+voronoi(10).color(0,() => a.fft[0] *2,1).out(o0)
+noise(() => a.fft[1] *200).rotate(1,1).color(1,0,0.5).out(o1)
+osc(40).color(0,1,1).scale( () => a.fft[2] *2 ).out(o2)
+shape(7).color(0.5,0,1).scale( () => a.fft[3] *2 ).out(o3)
+render()
+
 
 //Music: https://www.bensound.com
